3ec680cf83b22ff2aacdf042f6f0a3eda778b5ba
Ivan Gorbunov <i.gorbunov@corp.101xp.com>
2018-11-25 19:36:57 +0300

    upd fix

--- js/main.js
+++ js/main.js
@@ -15,52 +15,60 @@ const selectSlide = (element) => {
 const screens = Array.from(document.querySelectorAll(`template`)).
   map((it) => it.content);
 
-let currentScreen = 0;
+let currentScreenNumber = 0;
 const changeScreen = (index) => {
   index = index < 0 ? screens.length - 1 : index;
   index = index >= screens.length ? 0 : index;
-  currentScreen = index;
-  selectSlide(screens[currentScreen]);
+  currentScreenNumber = index;
+  selectSlide(screens[currentScreenNumber]);
 };
 
 document.addEventListener(`keydown`, (evt) => {
   switch (evt.keyCode) {
     case KeyCode.RIGHT_ARROW:
-      changeScreen(currentScreen + 1);
+      changeScreen(currentScreenNumber + 1);
       break;
     case KeyCode.LEFT_ARROW:
-      changeScreen(currentScreen - 1);
+      changeScreen(currentScreenNumber - 1);
       break;
   }
 });
 changeScreen(FIRST_SCREEN);
 
-const wrapper = document.createElement(`div`);
-wrapper.className = `arrows__wrap`;
-wrapper.innerHTML = `<style>
-.arrows__wrap {
-  position: absolute;
-  top: 95px;
-  left: 50%;
-  margin-left: -56px;
-}
-.arrows__btn {
-  background: none;
-  border: 2px solid black;
-  padding: 5px 20px;
-}
-</style>`;
-const buttonLeft = document.createElement(`button`);
-buttonLeft.innerHTML = `<-`;
-buttonLeft.className = `arrows__btn`;
-buttonLeft.addEventListener(`click`, () => {
-  changeScreen(currentScreen - 1);
+const arrowsWrapper = document.createElement(`div`);
+arrowsWrapper.className = `arrows__wrap`;
+arrowsWrapper.innerHTML = `
+<style>
+    .arrows__wrap {
+      position: absolute;
+      top: 95px;
+      left: 50%;
+      margin-left: -56px;
+    }
+    .arrows__btn {
+      background: none;
+      border: 2px solid black;
+      padding: 5px 20px;
+    }
+  </style>
+  <button class="arrows__btn" leftControlArrow><-</button>
+  <button class="arrows__btn" rightControlArrow>-></button>
+`;
+
+document.body.appendChild(arrowsWrapper);
+
+const controlArrows = document.getElementsByClassName(`arrows__btn`);
+
+Array.from(controlArrows).forEach((el) => {
+  if (el.hasAttribute(`leftControlArrow`)) {
+    el.addEventListener(`click`, () => {
+      changeScreen(currentScreenNumber - 1);
+    });
+  }
+  if (el.hasAttribute(`rightControlArrow`)) {
+    el.addEventListener(`click`, () => {
+      changeScreen(currentScreenNumber + 1);
+    });
+  }
 });
-const buttonRight = buttonLeft.cloneNode(true);
-buttonRight.innerHTML = `->`;
-buttonRight.addEventListener(`click`, () => {
-  changeScreen(currentScreen + 1);
-});
-document.body.appendChild(wrapper);
-wrapper.appendChild(buttonLeft);
-buttonLeft.parentNode.insertBefore(buttonRight, buttonLeft.nextSibling);
+
