a5e4a1811269470c6482e9bb3735acb520bd33a7
Ivan Gorbunov <i.gorbunov@corp.101xp.com>
2018-12-12 12:16:52 +0300

    upd

--- js/main.js
+++ js/main.js
@@ -1,74 +1,96 @@
-'use strict';
+import { changeScreen } from './util.js';
 
-const KeyCode = {
-  RIGHT_ARROW: 39,
-  LEFT_ARROW: 37
-};
-const FIRST_SCREEN = 0;
-const mainElement = document.querySelector(`#main`);
+changeScreen(`kek`);
 
-const selectSlide = (element) => {
-  mainElement.innerHTML = ``;
-  mainElement.appendChild(element.cloneNode(true));
-};
+// const arrowsWrapper = document.createElement(`div`);
+// arrowsWrapper.className = `arrows__wrap`;
+// arrowsWrapper.innerHTML = `
+// <style>
+//     .arrows__wrap {
+//       position: absolute;
+//       top: 95px;
+//       left: 50%;
+//       margin-left: -56px;
+//     }
+//     .arrows__btn {
+//       background: none;
+//       border: 2px solid black;
+//       padding: 5px 20px;
+//     }
+//   </style>
+//   <button class="arrows__btn" leftControlArrow><-</button>
+//   <button class="arrows__btn" rightControlArrow>-></button>
+// `;
 
-const screens = Array.from(document.querySelectorAll(`template`)).
-  map((it) => it.content);
+// const KeyCode = {
+//   RIGHT_ARROW: 39,
+//   LEFT_ARROW: 37
+// };
+// const FIRST_SCREEN = 0;
+// const mainElement = document.querySelector(`#main`);
 
-let currentScreenNumber = 0;
-const changeScreen = (index) => {
-  index = index < 0 ? screens.length - 1 : index;
-  index = index >= screens.length ? 0 : index;
-  currentScreenNumber = index;
-  selectSlide(screens[currentScreenNumber]);
-};
+// const selectSlide = (element) => {
+//   mainElement.innerHTML = ``;
+//   mainElement.appendChild(element.cloneNode(true));
+// };
 
-document.addEventListener(`keydown`, (evt) => {
-  switch (evt.keyCode) {
-    case KeyCode.RIGHT_ARROW:
-      changeScreen(currentScreenNumber + 1);
-      break;
-    case KeyCode.LEFT_ARROW:
-      changeScreen(currentScreenNumber - 1);
-      break;
-  }
-});
-changeScreen(FIRST_SCREEN);
+// const screens = Array.from(document.querySelectorAll(`template`)).
+//   map((it) => it.content);
 
-const arrowsWrapper = document.createElement(`div`);
-arrowsWrapper.className = `arrows__wrap`;
-arrowsWrapper.innerHTML = `
-<style>
-    .arrows__wrap {
-      position: absolute;
-      top: 95px;
-      left: 50%;
-      margin-left: -56px;
-    }
-    .arrows__btn {
-      background: none;
-      border: 2px solid black;
-      padding: 5px 20px;
-    }
-  </style>
-  <button class="arrows__btn" leftControlArrow><-</button>
-  <button class="arrows__btn" rightControlArrow>-></button>
-`;
+// let currentScreenNumber = 0;
+// const changeScreen = (index) => {
+//   index = index < 0 ? screens.length - 1 : index;
+//   index = index >= screens.length ? 0 : index;
+//   currentScreenNumber = index;
+//   selectSlide(screens[currentScreenNumber]);
+// };
 
-document.body.appendChild(arrowsWrapper);
+// document.addEventListener(`keydown`, (evt) => {
+//   switch (evt.keyCode) {
+//     case KeyCode.RIGHT_ARROW:
+//       changeScreen(currentScreenNumber + 1);
+//       break;
+//     case KeyCode.LEFT_ARROW:
+//       changeScreen(currentScreenNumber - 1);
+//       break;
+//   }
+// });
+// changeScreen(FIRST_SCREEN);
 
-const controlArrows = document.getElementsByClassName(`arrows__btn`);
+// const arrowsWrapper = document.createElement(`div`);
+// arrowsWrapper.className = `arrows__wrap`;
+// arrowsWrapper.innerHTML = `
+// <style>
+//     .arrows__wrap {
+//       position: absolute;
+//       top: 95px;
+//       left: 50%;
+//       margin-left: -56px;
+//     }
+//     .arrows__btn {
+//       background: none;
+//       border: 2px solid black;
+//       padding: 5px 20px;
+//     }
+//   </style>
+//   <button class="arrows__btn" leftControlArrow><-</button>
+//   <button class="arrows__btn" rightControlArrow>-></button>
+// `;
 
-Array.from(controlArrows).forEach((el) => {
-  if (el.hasAttribute(`leftControlArrow`)) {
-    el.addEventListener(`click`, () => {
-      changeScreen(currentScreenNumber - 1);
-    });
-  }
-  if (el.hasAttribute(`rightControlArrow`)) {
-    el.addEventListener(`click`, () => {
-      changeScreen(currentScreenNumber + 1);
-    });
-  }
-});
+// document.body.appendChild(arrowsWrapper);
+
+// const controlArrows = document.getElementsByClassName(`arrows__btn`);
+
+// Array.from(controlArrows).forEach((el) => {
+//   if (el.hasAttribute(`leftControlArrow`)) {
+//     el.addEventListener(`click`, () => {
+//       changeScreen(currentScreenNumber - 1);
+//     });
+//   }
+//   if (el.hasAttribute(`rightControlArrow`)) {
+//     el.addEventListener(`click`, () => {
+//       changeScreen(currentScreenNumber + 1);
+//     });
+//   }
+// });
 
